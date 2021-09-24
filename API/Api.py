0a4b41ad288444f330a9c307ff8049f666c12582
YJ <oooo1463@gmail.com>
2021-09-24 15:26:47 +0900

    Update api.py f string format

--- API/Api.py
+++ API/Api.py
@@ -8,16 +8,18 @@ user_key = User_key
 
 
 class Api:
+    @staticmethod
     def get_auth_token(problem, num_of_elevators):
-        url = base_url + '/' + 'start/' + user_key + '/' + str(problem) + '/' + str(num_of_elevators)
+        url = f'{base_url}/start/{user_key}/{problem}/{num_of_elevators}'
         try:
             response_start = requests.post(url).json()
         except:
             return None
         return response_start
 
+    @staticmethod
     def oncall(auth_token):
-        url = base_url + '/' + 'oncalls'
+        url = f'{base_url}/oncalls'
         headers = {'X-Auth-Token': auth_token}
         try:
             res_oncall = requests.get(url, headers=headers).json()
@@ -25,8 +27,9 @@ class Api:
             return None
         return res_oncall
 
+    @staticmethod
     def action(auth_token, commands):
-        url = base_url + '/' + 'action'
+        url = f'{base_url}/action'
         headers = {'Content-Type': 'application/json; charset=utf-8', 'X-Auth-Token': auth_token}
 
         try:
